5716b4f18ec9bcda2b05ea653a4b221009d701e0
Puneet Singh <91puneetsingh@gmail.com>
2026-01-28 11:28:18 +0000

    monitoring_agent

---
+++ linux_sql/scripts/sql/ddl.sql
@@ -0,0 +1,28 @@
+-- Switch to host_agent database is assumed done by psql -d host_agent
+-- Create host_info table if it does not exist
+CREATE TABLE IF NOT EXISTS host_info
+(
+    id               SERIAL PRIMARY KEY,
+    hostname         VARCHAR NOT NULL UNIQUE,
+    cpu_number       INT2 NOT NULL,
+    cpu_architecture VARCHAR NOT NULL,
+    cpu_model        VARCHAR NOT NULL,
+    cpu_mhz          FLOAT8 NOT NULL,
+    l2_cache         INT4 NOT NULL,
+    "timestamp"      TIMESTAMP,
+    total_mem        INT4
+);
+
+-- Create host_usage table if it does not exist
+CREATE TABLE IF NOT EXISTS host_usage
+(
+    "timestamp"    TIMESTAMP NOT NULL,
+    host_id        INT NOT NULL,
+    memory_free    INT4 NOT NULL,
+    cpu_idle       INT2 NOT NULL,
+    cpu_kernel     INT2 NOT NULL,
+    disk_io        INT4 NOT NULL,
+    disk_available INT4 NOT NULL,
+    CONSTRAINT host_usage_host_info_fk FOREIGN KEY (host_id) REFERENCES host_info(id)
+);
+
